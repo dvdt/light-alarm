3116bf27bd42c7d734b625040fac9b8f0e821455
David Tsao <davidtsao@gmail.com>
2014-03-19 23:14:49 -0500

    api seems ready; sketched out some gpio code

--- src/light_alarm/handler.clj
+++ src/light_alarm/handler.clj
@@ -5,10 +5,11 @@
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.json :as json])
+  (:import [com.pi4j.io.gpio GpioController GpioFactory GpioPinDigitalOutput
+            PinState RaspiPin])
   (:gen-class))
 
-(def alarm-time (ref 0))
-
+(def alarm-time (ref {:hr 7 :min 30 :meridian "am"}))
 (def pwm-status (ref 0))
 
 (defroutes app-routes
@@ -20,12 +21,22 @@
   (GET "/status" [] {:body  {:pwm_status (deref pwm-status)}})
   (POST "/status" [pwm_status]
         (dosync (ref-set pwm-status pwm_status))
-        {:body  {:pwm_status (deref pwm-status)}}))
+        {:body  {:pwm_status (deref pwm-status)}})
+  (GET "/alarm" [] {:body (deref alarm-time)})
+  (POST "/alarm" [& p]
+        (dosync (ref-set alarm-time (conj (deref alarm-time) p)))
+        {:body   (deref alarm-time)}))
 
 (def app
   (handler/site (routes (->  api-routes json/wrap-json-params
                              json/wrap-json-response)
                         app-routes)))
+(defn init-pwm
+  "Provsions pin1 (PWM pin) on raspberry pi and sets output to pwm-init. Returns
+   instance of GpioPinPwmOutput"
+  [pwm-init]
+  (let [gpio (GpioFactory/getInstance)]
+    (. gpio provisionPwmOutputPin  RaspiPin/GPIO_01  "MyLED" pwm-init)))
 
 (defn -main  [& [port]]
       (let [port (Integer. (or port (System/getenv "PORT") 5000))]
